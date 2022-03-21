f51dba53832ab9f03e96bf02bcc9f2e2f82303e7
Nemanja <nemanja.galbinovic@badin.rs>
2022-03-21 23:35:46 +0100

    add news fix

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ const news = document.querySelector('[data-news]');
 export const displayNews = () => {
     fetchNewsData().then(data => {
         for (let i = 0; i < data.length; i++) {
-            const { title, category, time, prefix } = data[i];
+            const { title, category } = data[i];
 
             news.innerHTML += `
             <div class="news-section">
             <div class="news-timeline">
-                <p class="news-time-posted">${time}<span>${prefix}</span></p>
+                <p class="news-time-posted">${i+1}<span>min</span></p>
             </div>
             <div class="image-wrapper">
                 <img src="assets/pictures/news.png" class="image-thumbnail" alt="news">
